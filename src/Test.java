2001649a8cfeda63432ddca071d90da07067694e
erudite-chirag <161641443+erudite-chirag@users.noreply.github.com>
2024-03-08 15:48:26 -0500

    add Test class (implements Validator)

---
+++ src/Test.java
@@ -0,0 +1,9 @@
+public class Test implements Validator {
+
+    @Override
+    public boolean pass_length() {
+
+        throw new UnsupportedOperationException("Unimplemented method 'pass_length'");
+    }
+
+}
