a83699ebdbc5b8ffa23d7f7141e70be6bd949eb5
erudite-chirag <161641443+erudite-chirag@users.noreply.github.com>
2024-03-08 15:48:55 -0500

    add pass_length() to App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class App {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        System.out.println("PASSWORD VALIDATOR");
+        Input in = new Input();
+        final String user = in.get_user();
+        final String pass = in.get_pass();
+        boolean flag = true;
+        App obj = new App();
+
+        if (flag) {
+            boolean len = obj.pass_length(pass);
+            if (!len) {
+                System.out.println("Password Invalid! Minimum 12 characters required");
+                flag = false;
+            }
+        }
+
+        // System.out.println(user + " " + pass);
+
     }
+
+    public boolean pass_length(String pass) {
+        int len = pass.length();
+        if (len < 12)
+            return false;
+
+        return true;
+    }
+
 }
