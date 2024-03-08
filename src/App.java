39a0a9d9831dffec1211e13f49ef69edad037524
erudite-chirag <161641443+erudite-chirag@users.noreply.github.com>
2024-03-08 15:52:54 -0500

    add pass_max_length() to App.java

--- src/App.java
+++ src/App.java
@@ -12,9 +12,16 @@ public class App {
         App obj = new App();
 
         if (flag) {
-            boolean len = obj.pass_length(pass);
-            if (!len) {
+            boolean min_len = obj.pass_min_length(pass);
+            if (!min_len) {
                 System.out.println("Password Invalid! Minimum 12 characters required");
+                flag = false;
+            }
+        }
+        if (flag) {
+            boolean max_len = obj.pass_max_length(pass);
+            if (!max_len) {
+                System.out.println("Password Invalid! Maximum 20 characters.");
                 flag = false;
             }
         }
@@ -23,11 +30,17 @@ public class App {
 
     }
 
-    public boolean pass_length(String pass) {
+    public boolean pass_min_length(String pass) {
         int len = pass.length();
         if (len < 12)
             return false;
+        return true;
+    }
 
+    public boolean pass_max_length(String pass) {
+        int len = pass.length();
+        if (len > 20)
+            return false;
         return true;
     }
 
